d764710127dadf086b1845a367818dc7bc811adb
kevin-shen-nti-johanneberg <kevin.shen@elev.ga.ntig.se>
2022-03-02 16:39:24 +0100

    Databas formulär
    Gjorde som i rubrikn

--- app.rb
+++ app.rb
@@ -5,14 +5,42 @@ require 'sqlite3'
 enable :sessions
 
 post('') do
+    #Skapa koppling till databasen
+    db = SQLite3::Database.new("db/quiz.db")
+    #Få svar i strukturen
+    db.results_as_hash = true
+    #Hämta data, skicka data
+    result = db.execute("SELECT * FROM users")
+    slim(:users,locals:{key:result})
     redirect('/index')
 end
 
 post('/login') do
-    redirect('/login')
+    name = params[:player_name]    
+    pass = params[:password]
+
+    db = SQLite3::Database.new("db/quiz.db")
+    db.results_as_hash = true
+
+    if db.execute("SELECT password FROM player WHERE player_name = ?",name).first["password"] == pass        
+        p "DET FUNKAR!"
+        redirect('/profile')
+    end
+
 end
 
 post('/register') do
+    #Hämta data ifrån formulär
+
+    name = params[:player_name]    
+    pass = params[:password]
+
+
+    #Lägg till data i databasen
+
+    db = SQLite3::Database.new("db/quiz.db")
+    db.execute("INSERT INTO player (player_name,password) VALUES (?,?)",name,pass)
+
     redirect('/register')
 end
 
